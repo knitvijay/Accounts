b08de0d82c37e1b46a48d816ea6d1fde25ab1c48
vijay <knitvijay@gmail.com>
2019-08-01 23:55:32 +0530

    changes

---
+++ Accounts.Tests/AccountingCalculatorTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Accounts.BLL;
+using NUnit.Framework;
+
+namespace Accounts.Tests
+{
+    [TestFixture]
+    public class AccountingCalculatorTests
+    {
+        [Test]
+        public void CalculateProfit()
+        {
+            IAccountingCalculator calc = new AccountingCalculator();
+            decimal net = calc.CalculateNet(1000m, 1000m);
+
+            Assert.AreEqual(0m, net);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeRevenue()
+        {
+            IAccountingCalculator calc = new AccountingCalculator();
+            decimal net = calc.CalculateNet(-1000m, 1000m);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativeExpenses()
+        {
+            IAccountingCalculator calc = new AccountingCalculator();
+            decimal net = calc.CalculateNet(1000m, -1000m);
+        }
+    }
+}
---
+++ Accounts.Web/Site.Master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Web.UI;
+using Accounts.BLL;
+
+namespace Accounts.Web
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            litWebConfigValue.Text = WebConfigurationManager.AppSettings["Accounts.Value"];
+
+            try
+            {
+                litVersion.Text = typeof(AccountingCalculator).Assembly.GetName().Version.ToString();
+            }
+            catch { }
+        }
+    }
+}
